3ee33b7973fbc1ff6fbfeb784459e8922986d815
daeunLee <eleeje97@gmail.com>
2020-05-16 16:13:30 +0900

    contents copy button add

--- auto_install/app/src/main/java/com/example/auto_install/ContentsCopyActivity.java
+++ auto_install/app/src/main/java/com/example/auto_install/ContentsCopyActivity.java
@@ -3,12 +3,17 @@ package com.example.auto_install;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class ContentsCopyActivity extends AppCompatActivity {
+
+    Button button; // 컨텐츠 복사 버튼
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contents_copy);
+
+        button = findViewById(R.id.button);
     }
 }
