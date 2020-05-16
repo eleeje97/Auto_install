3c4daef870625b9637e33f3fab5b1c84b66a545e
daeunLee <eleeje97@gmail.com>
2020-05-16 15:19:33 +0900

    create ContentsCopyActivity

---
+++ auto_install/app/src/main/java/com/example/auto_install/ContentsCopyActivity.java
@@ -0,0 +1,14 @@
+package com.example.auto_install;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ContentsCopyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contents_copy);
+    }
+}
