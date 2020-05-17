0d27e65dd7ff966b077b0797b099532793fe2f25
unknown <shyunju7@naver.com>
2020-05-17 20:54:35 +0900

    modify mainactivity and manifests

--- auto_install/app/src/main/java/com/example/auto_install/MainActivity.java
+++ auto_install/app/src/main/java/com/example/auto_install/MainActivity.java
@@ -3,23 +3,33 @@ package com.example.auto_install;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.hardware.usb.UsbAccessory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.StrictMode;
+import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button contents_btn;
-    private TextView text;
-    private String log = ""; // 로그를 찍을 내용
+
+    private TextView apk1,apk2,apk3; // 3가지 apk 파일의 설치 여부를 출력
+    private Button contents_btn; // 콘텐츠 다운로드 버튼
+    private boolean all_apks_install = false; // 3개의 apk 파일이 설치 되었는지 여부
+
     private String apk_file_name =  "";
+
+    private boolean check = false; // 해당 앱이 설치되어 있는지 확인
     /**
      * 송현주 USB 경로 : /storage/5029-C52A
      * */
@@ -33,14 +43,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        text = findViewById(R.id.text);
+
+        // apk 파일 접근해 설치를 할 수 있도록
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
+
+        // xml 파일 연결
+        apk1 = findViewById(R.id.apkinfo1);
+        apk2 = findViewById(R.id.apkinfo2);
+        apk3 = findViewById(R.id.apkinfo3);
+
+
+
         contents_btn = findViewById(R.id.contents_btn);
-
-
         contents_btn.setOnClickListener(new View.OnClickListener(){
 
             @Override
-            public void onClick(View v) {
+            public void onClick(View v) { // 텍스트 다운로드 버튼을 클릭했다면
+
 
                 Intent intent = new Intent(getApplicationContext(),ContentsCopyActivity.class);
 
@@ -48,33 +68,59 @@ public class MainActivity extends AppCompatActivity {
                 File usb = new File(usb_path);
                 File[] usbFiles = usb.listFiles();
 
+                // usb 파일 없다면 null 있다면 하위 폴더 정보 저장
                 if(usbFiles == null){
-                    text.setText("NULL!!!!");
+                    Toast.makeText(getApplication(), "null", Toast.LENGTH_SHORT).show();
                 }
 
                 else {
                     // usbFiles 안에 리스트를 출력
                     // apks라는 이름을 가진 폴더를 찾아 저장
+
+                    // usb 하위 폴더 중 apk 파일이 담겨 있는 폴더를 찾음
                     for (File curFile : usbFiles) {
 
+                        // 현재 폴더의 이름이 apks 라면
                         if (curFile.getName().equals("apks")) {
-                            apk_file_name += curFile.getName();
-                        } else {
+                            apk_file_name = curFile.getName();
+                        }
+                        // 일치하는 폴더가 없다면
+                        else {
                             System.out.println("Not Found 'apks'");
                         }
-
                     }
 
+                    // 위 과정을 통해 일치하는 폴더를 찾았다면 apks 파일 안에 각각의 apk파일을 가져오기 위해 다시 파일 생성
+                    // apk_file_name이 apks임, usb안에 apks 파일을 apk_dir로 표현한 것임
+                    File apk_dir = new File(usb_path + "/" +apk_file_name);
+
                     // apks 파일 안에 apk 파일 path 저장
-                    File apk_dir = new File(usb_path + apk_file_name);
-                    //File[] apkFiles = apk_dir.listFiles();
+                    File[] apkFiles = apk_dir.listFiles();
 
-                    //for(File apkFile : apkFiles){
-                    //    log += apkFile;
-                    //}
+                    /** 기기에 설치된 앱의 packageInfo 확인하기 **/
+                    // 나중에 example 회사명이나 다른 걸로 바꾸기
+                    String auto_install_pk = "com.example.auto_install";
+                    if(isChecked(auto_install_pk) == true){
 
-                    text.setText(">> " + apk_file_name
-                            + "\n apk_dir path :  >>" + apk_dir.getPath()+"\n");
+                        // akps 파일에 3개의 apk 파일을 설치
+                        for(File apkFile : apkFiles){
+                            apkInstall(apkFile);
+                        }
+
+
+                        //  3개의 apk 파일이 설치되어 패키지가 존재하는지 확인
+                        
+                    }
+                    else{
+                        Toast.makeText(getApplication(), auto_install_pk + "앱이 설치되어 있지 않습니다."
+                                , Toast.LENGTH_SHORT).show();
+                    }
+
+
+
+
+
+
                 }
                 startActivity(intent);
 
@@ -103,6 +149,44 @@ public class MainActivity extends AppCompatActivity {
          *  이유) 보안상의 이유로 apk 파일로 설치하는 것이 허용되어 있지 않음으로 사용자가 직접 체크 해주어야 함 **/
 
         // 3가지 apk 파일들의 설치 여부 확인 후, 콘텐츠 다운로드 버튼 활성화
+    }
+
+
+    // apkFile 자동 설치를 위한 메소드
+    public void apkInstall(File apkFile){
+
+        // 설치할 apk 파일의 위치를 저장
+        Uri apkUri = Uri.fromFile(apkFile);
+
+        try{
+            Intent apk_intent = new Intent(Intent.ACTION_VIEW);
+            apk_intent.setDataAndType(apkUri, "application/vnd.android.package-archive");
+
+
+            startActivity(apk_intent);
+        }catch (Exception e){
+            e.printStackTrace();
+
+        }
+    }
+
+
+    // 앱의 설치여부 확인을 위한 메소드
+    public boolean isChecked(String packagename){
+        Intent launchIntent = getPackageManager().getLaunchIntentForPackage(packagename);
+
+        // 해당 패키지 존재 여부에 따라 동작하도록
+        // 해당 앱이 설치되어 있지 않다면
+        if(launchIntent == null){
+            return false;
+        }
+
+        else { // 앱이 설치되어 있다면 Toast 메세지 출력
+            Toast.makeText(getApplication(), packagename + "앱이 설치되어 있습니다."
+                    , Toast.LENGTH_SHORT).show();
+
+            return true;
+        }
     }
 }
 
