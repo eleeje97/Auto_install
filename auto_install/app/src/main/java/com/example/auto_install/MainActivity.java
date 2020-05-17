3b67a8460a72f9474457beb04c2a6ea165a7d930
unknown <shyunju7@naver.com>
2020-05-17 15:57:37 +0900

    modify MainActivity

--- auto_install/app/src/main/java/com/example/auto_install/MainActivity.java
+++ auto_install/app/src/main/java/com/example/auto_install/MainActivity.java
@@ -3,40 +3,94 @@ package com.example.auto_install;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.hardware.usb.UsbAccessory;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button contents_btn;
+    private TextView text;
+    private String log = ""; // 로그를 찍을 내용
+    private String apk_file_name =  "";
+    /**
+     * 송현주 USB 경로 : /storage/5029-C52A
+     * */
+
     // usb > apkfiles 디렉토리의 경로 지정
-    public String dir_path = Environment.getExternalStorageDirectory().getAbsolutePath()
-                             + "디렉토리 이름 지정`";
+    public String usb_path = "/storage/5029-C52A";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        text = findViewById(R.id.text);
+        contents_btn = findViewById(R.id.contents_btn);
 
-        // 지정된 경로의 파일 생성
-        //File dir_file = new File(dir_path);
+
+        contents_btn.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(getApplicationContext(),ContentsCopyActivity.class);
+
+                // usb의 경로 저장 및 usb에 담긴 리스트를 저장할 파일 생성
+                File usb = new File(usb_path);
+                File[] usbFiles = usb.listFiles();
+
+                if(usbFiles == null){
+                    text.setText("NULL!!!!");
+                }
+
+                else {
+                    // usbFiles 안에 리스트를 출력
+                    // apks라는 이름을 가진 폴더를 찾아 저장
+                    for (File curFile : usbFiles) {
+
+                        if (curFile.getName().equals("apks")) {
+                            apk_file_name += curFile.getName();
+                        } else {
+                            System.out.println("Not Found 'apks'");
+                        }
+
+                    }
+
+                    // apks 파일 안에 apk 파일 path 저장
+                    File apk_dir = new File(usb_path + apk_file_name);
+                    //File[] apkFiles = apk_dir.listFiles();
+
+                    //for(File apkFile : apkFiles){
+                    //    log += apkFile;
+                    //}
+
+                    text.setText(">> " + apk_file_name
+                            + "\n apk_dir path :  >>" + apk_dir.getPath()+"\n");
+                }
+                startActivity(intent);
 
 
-        // 생성된 dir_file의 하위 파일들(확장자: .apk) 경로를 apkFiles에 저장
 
-        //File[] apkFiles = dir_file.listFiles();
+            }
 
-        /** 정보를 잘 가져 왔는지 확인용
-         *  테스트 해보고 아래 for문은 지울 것 **/
 
-        /*
-        for(File curFile: apkFiles){
-            System.out.println(curFile.getPath());
-        }
-        */
+
+        });
+
+
+
+
+
+
 
         // auto_install 앱이 설치 되었는지를 확인
         // packageManager를 이용하면 확인 가능
